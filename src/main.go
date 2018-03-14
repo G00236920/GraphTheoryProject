1dfd2b4804c2da2c71b2852c3664fa9aded8090e
Michael Kidd <G00236920@gmit.ie>
2018-03-14 14:51:44 +0000

    moving to desktop

--- src/main.go
+++ src/main.go
@@ -4,12 +4,17 @@ func main() {
 
 }
 
+//Shunting Yard algorithm
 //Change the string from infix to postfix notation
 func intoPost(infix string) string {
 
-	specials := map[rune]int{"*": 10, ".": 9, "|": 8}
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 
 	postfix, s := []rune{}, []rune{}
+
+	for _, r := range infix {
+
+	}
 
 	return string(postfix)
 
