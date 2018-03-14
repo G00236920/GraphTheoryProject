c0bcd7b9fa9cb7b2405c4513f9fa65810c1a8515
Michael Kidd <G00236920@gmit.ie>
2018-03-14 10:23:21 +0000

    postfix function added

--- src/main.go
+++ src/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
-
 func main() {
-	
+
+}
+
+//Change the string from infix to postfix notation
+func intoPost(infix string) string {
+
+	specials := map[rune]int{"*": 10, ".": 9, "|": 8}
+
+	postfix, s := []rune{}, []rune{}
+
+	return string(postfix)
+
 }
