b2e08024e499af2286ac8f85793673bf6f3062c4
Michael Kidd <G00236920@gmit.ie>
2018-03-15 09:37:53 +0000

    Shunting yard algorithm added and understood

--- src/main.go
+++ src/main.go
@@ -8,11 +8,70 @@ func main() {
 //Change the string from infix to postfix notation
 func intoPost(infix string) string {
 
+
 	specials := map[rune]int{'*': 10, '.': 9, '|': 8}
 
 	postfix, s := []rune{}, []rune{}
 
 	for _, r := range infix {
+
+		switch {
+
+			case r == '(':
+				
+				//Add r to the stack
+				s = append(s, r) 
+
+			case r == ')':
+
+				//pop off the stack until the opening bracket is located
+				//Loop while the last character is not the opening bracket
+				for s[len(s)-1] != '('
+				{
+
+					//Add the top of the stack to postfix
+					postfix = append(postfix,  s[len(s)-1])
+
+					//add everything except the last element to the stack
+					//Removing the last element as we have appended it to the postfix rune
+					s = s[:len(s)-1]
+
+				}
+
+			case specials[r] > 0:
+
+				for len(s) > 0 && specials[r] <= specials[s[len(s)-1]]
+				{
+
+					//Add the top of the stack to postfix
+					postfix = append(postfix,  s[len(s)-1])
+
+					//add everything except the last element to the stack
+					//Removing the last element as we have appended it to the postfix rune
+					s = s[:len(s)-1]
+
+				}
+				
+				s = append(s, r) 
+
+			default:
+				
+				//Append any other character to the postfix rune
+				postfix = append(postfix r)
+
+		}
+
+	}
+
+	if len(s) > 0
+	{
+
+		//Add the top of the stack to postfix
+		postfix = append(postfix,  s[len(s)-1])
+
+		//add everything except the last element to the stack
+		//Removing the last element as we have appended it to the postfix rune
+		s = s[:len(s)-1]
 
 	}
 
